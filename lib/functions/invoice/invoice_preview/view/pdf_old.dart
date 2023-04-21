30ad1e951d824f9dad5ee9e0f1208f88017fea9f
Gani Myrzamuratov <gmyrzamuratov@gmail.com>
2023-04-22 00:42:23 +0600

    PDF added

--- lib/functions/invoice/invoice_preview/view/pdf_old.dart
+++ lib/functions/invoice/invoice_preview/view/pdf_old.dart
@@ -1,5 +1,6 @@
 import 'dart:typed_data';
 
+import 'package:intl/intl.dart';
 import 'package:karzhy_frontend/models/invoice.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart';
@@ -7,20 +8,22 @@ import 'package:printing/printing.dart';
 
 class PdfInvoiceOldApi {
   static Future<Uint8List> generate(Invoice invoice) async {
-
     var nunitoRegular = await PdfGoogleFonts.nunitoRegular();
     var nunitoBold = await PdfGoogleFonts.nunitoBold();
 
+    final f = DateFormat('dd.MM.yyyy');
+    final created = f.format(invoice.created);
+
     final titles = <String>['Күні:'];
     final data = <String>[
-      invoice.created.toString(),
+      created,
     ];
-    final headers = ['№', 'Аты', 'Саны', 'Өлш', 'Бағасы', 'Сомасы'];
+    final headers = ['№', 'Тауар', 'Саны', 'Бірл.', 'Бағасы', 'Сомасы'];
     //final invoices_data = [...invoice.products.map((e) => e.toList()).toList()];
 
     List<List<dynamic>> invoices_data = [];
 
-    for(int i=0; i<invoice.products.length; i++) {
+    for (int i = 0; i < invoice.products.length; i++) {
       List<dynamic> row = [
         i,
         invoice.products[i].name,
@@ -34,6 +37,7 @@ class PdfInvoiceOldApi {
 
     final pdf = Document();
     pdf.addPage(MultiPage(
+      header: null,
       /*header: (context) => Text("Төлем шоты N ${invoice.number}",
           style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold, font: nunitoBold)),*/
       build: (context) => [
@@ -54,8 +58,22 @@ class PdfInvoiceOldApi {
                       height: 80,
                       width: 80,
                     )
-                        : */Text(""),
+                        : */
+                    Text(""),
                     SizedBox(height: 1 * PdfPageFormat.cm),
+                    Text(
+                        "Назар аударыңыздар! Берілген шотты төлеу - тауарды жеткізу шарттарымен келісуді білдіреді. Төлем",
+                        style: TextStyle(font: nunitoRegular, fontSize: 8.0),
+                        textAlign: TextAlign.right),
+                    Text(
+                        "туралы хабарламаның болуы міндетті, болмаған жағдайда тауардың қоймада болуына кепілдік берілмейді.",
+                        style: TextStyle(font: nunitoRegular, fontSize: 8.0)),
+                    Text(
+                        "Тауар Жеткізушінің е/ш ақшаның түсу фактісі бойынша, өзі алып кету арқылы, сенімхат және төлқұжат болған",
+                        style: TextStyle(font: nunitoRegular, fontSize: 8.0)),
+                    Text("жағдайда жіберіледі.",
+                        style: TextStyle(font: nunitoRegular, fontSize: 8.0)),
+                    /*
                     Text("Внимание! Оплата данного счета означает согласие с условиями поставки товара.",
                         style: TextStyle(font: nunitoRegular)),
                     Text("Уведомление об оплате обязательно, в противном случае не гарантируется наличие",
@@ -63,12 +81,7 @@ class PdfInvoiceOldApi {
                     Text("товара на складе. Товар отпускается по факту прихода денег на р/с Поставщика,",
                         style: TextStyle(font: nunitoRegular)),
                     Text("самовывозом, при наличии доверенности и документов удостоверяющих личность.",
-                        style: TextStyle(font: nunitoRegular)),
-                    /*
-                    Text(invoice.company.name!,
-                        style: TextStyle(fontWeight: FontWeight.bold, font: nunitoBold)),
-                    SizedBox(height: 1 * PdfPageFormat.mm),
-                    Text(invoice.company.address!, style: TextStyle(font: nunitoRegular)),*/
+                        style: TextStyle(font: nunitoRegular)),*/
                   ],
                 ),
                 /*
@@ -84,102 +97,140 @@ class PdfInvoiceOldApi {
             ),
             SizedBox(height: 1 * PdfPageFormat.cm),
             Table(
-              border: TableBorder.all(color: PdfColors.black),
-              children: [
-                TableRow(children: [
-                  Text('Бенефициар:\n'
-                      '${invoice.company.name!}\n'
-                      '\n'
-                      'БИН:${invoice.company.bin}', style: TextStyle(font: nunitoRegular)),
-                  Text('ИИК\n'
-                      '\n'
-                      '${invoice.bankAccount.iban}', style: TextStyle(font: nunitoRegular)),
-                  Text('Кбе\n'
-                      ' \n'
-                      '${invoice.company.kbe}', style: TextStyle(font: nunitoRegular)),
+                columnWidths: {
+                  0: FlexColumnWidth(3),
+                  1: FlexColumnWidth(2),
+                  2: FlexColumnWidth(1),
+                },
+                border: TableBorder.all(color: PdfColors.black),
+                children: [
+                  TableRow(children: [
+                    Container(
+                        padding: const EdgeInsets.all(3.0),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Бенефициар:',
+                                  style: TextStyle(font: nunitoBold)),
+                              Text(invoice.company.name!,
+                                  style: TextStyle(font: nunitoBold)),
+                              Text('БСН:${invoice.company.bin}',
+                                  style: TextStyle(font: nunitoRegular))
+                            ])),
+                    Container(
+                        padding: const EdgeInsets.all(3.0),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text('ЖИК', style: TextStyle(font: nunitoBold)),
+                              Text(invoice.bankAccount.iban!,
+                                  style: TextStyle(font: nunitoBold))
+                            ])),
+                    Container(
+                        padding: const EdgeInsets.all(3.0),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text('Бек', style: TextStyle(font: nunitoBold)),
+                              Text('${invoice.company.kbe}',
+                                  style: TextStyle(font: nunitoBold))
+                            ])),
+                  ]),
                 ]),
-                TableRow(children: [
-                  Text('Банк бенефициара:\n'
-                      '${invoice.bankAccount.bank!.name}\n', style: TextStyle(font: nunitoRegular)),
-                  Text('БИК\n'
-                      '\n'
-                      '${invoice.bankAccount.bank!.swift}', style: TextStyle(font: nunitoRegular)),
-                  Text('Код назначения платежа', style: TextStyle(font: nunitoRegular)),
-                ])
-              ]
-            ),
-            SizedBox(height: 35.0),
-            Text("${invoice.created} күнгі төлем шоты № ${invoice.number} ", style: TextStyle(font: nunitoBold, fontSize: 20.0)),
-            Divider(),
             Table(
                 columnWidths: {
-                  0: FlexColumnWidth(1),
-                  1: FlexColumnWidth(3),
+                  0: FlexColumnWidth(3),
+                  1: FlexColumnWidth(1),
+                  2: FlexColumnWidth(2),
                 },
-              children: [
-                TableRow(
-                    children: [
-                      Text("Жеткізуші:          ", style: TextStyle(font: nunitoRegular)),
-                      Text("ЖСН:${invoice.company.bin}, ${invoice.company.name}, ${invoice.company.address}",
-                          style: TextStyle(font: nunitoBold))
+                border: TableBorder.all(color: PdfColors.black),
+                children: [
+                  TableRow(children: [
+                    Container(
+                        padding: const EdgeInsets.all(3.0),
+                        child: Text(
+                            'Бенефициар банкі:\n'
+                            '${invoice.bankAccount.bank!.name}\n',
+                            style: TextStyle(font: nunitoRegular))),
+                    Container(
+                        padding: const EdgeInsets.all(3.0),
+                        child: Column(children: [
+                          Text('БЖК', style: TextStyle(font: nunitoBold)),
+                          Text('${invoice.bankAccount.bank!.swift}',
+                              style: TextStyle(font: nunitoBold)),
+                        ])),
+                    Container(
+                        padding: const EdgeInsets.all(3.0),
+                        child: Column(children: [
+                          Text('Төлемді тағайындау коды',
+                              style: TextStyle(font: nunitoBold)),
+                        ])),
+                  ])
                 ]),
-                TableRow(
-                    children: [
-                      Container(
-                        height: 20.0,
-                        child: Text("")
-                      ),
-                      Container(
-                          child: Text("")
-                      ),
-                    ]),
-                TableRow(
-                    children: [
-                      Text("Сатып алушы:", style: TextStyle(font: nunitoRegular)),
-                      Text("ЖСН:${invoice.client.bin}, ${invoice.client.name}, ${invoice.client.address}",
-                          style: TextStyle(font: nunitoBold))
-                ]),
-                TableRow(
-                    children: [
-                      Container(
-                          height: 20.0,
-                          child: Text("")
-                      ),
-                      Container(
-                          child: Text("")
-                      ),
-                    ]),
-                TableRow(
-                    children: [
-                      Text("Келісім-шарт:", style: TextStyle(font: nunitoRegular)),
-                      Text(invoice.contract.name!,
-                          style: TextStyle(font: nunitoBold))
-                    ]),
-
-              ]
-            ),
+            SizedBox(height: 35.0),
+            Text("Төлем шоты № ${invoice.number} ${created}",
+                style: TextStyle(font: nunitoBold, fontSize: 20.0)),
+            Divider(),
+            Table(columnWidths: {
+              0: FlexColumnWidth(1),
+              1: FlexColumnWidth(3),
+            }, children: [
+              TableRow(children: [
+                Text("Жеткізуші:", style: TextStyle(font: nunitoRegular)),
+                Text(
+                    "${invoice.company.name}, ${invoice.company.address}",
+                    style: TextStyle(font: nunitoBold))
+              ]),
+              TableRow(children: [
+                Container(height: 20.0, child: Text("")),
+                Container(child: Text("")),
+              ]),
+              TableRow(children: [
+                Text("Сатып алушы:", style: TextStyle(font: nunitoRegular)),
+                Text(
+                    "ЖСН:${invoice.client.bin}, ${invoice.client.name}, ${invoice.client.address}",
+                    style: TextStyle(font: nunitoBold))
+              ]),
+              TableRow(children: [
+                Container(height: 20.0, child: Text("")),
+                Container(child: Text("")),
+              ]),
+              TableRow(children: [
+                Text("Келісім-шарт:", style: TextStyle(font: nunitoRegular)),
+                Text(invoice.contract.name!, style: TextStyle(font: nunitoBold))
+              ]),
+            ]),
           ],
         ),
         SizedBox(height: 5.0),
         Table.fromTextArray(
-          headers: headers,
-          data: invoices_data,
-          border: TableBorder.all(color: PdfColors.black),
-          headerStyle: TextStyle(fontWeight: FontWeight.bold, font: nunitoBold),
-          headerAlignment: Alignment.center,
-          //headerDecoration: BoxDecoration(color: PdfColors.grey300),
-          cellHeight: 30,
-          cellAlignments: {
-            0: Alignment.center,
-            1: Alignment.centerLeft,
-            2: Alignment.center,
-            3: Alignment.centerRight,
-            4: Alignment.centerRight,
-            5: Alignment.centerRight,
-            6: Alignment.centerRight,
-          },
-          cellStyle: TextStyle(font: nunitoRegular),
-        ),
+            headers: headers,
+            data: invoices_data,
+            border: TableBorder.all(color: PdfColors.black),
+            headerStyle:
+                TextStyle(fontWeight: FontWeight.bold, font: nunitoBold),
+            headerAlignment: Alignment.center,
+            //headerDecoration: BoxDecoration(color: PdfColors.grey300),
+            cellHeight: 30,
+            cellAlignments: {
+              0: Alignment.center,
+              1: Alignment.centerLeft,
+              2: Alignment.center,
+              3: Alignment.centerRight,
+              4: Alignment.centerRight,
+              5: Alignment.centerRight,
+              6: Alignment.centerRight,
+            },
+            cellStyle: TextStyle(font: nunitoRegular),
+            columnWidths: {
+              0: FlexColumnWidth(1),
+              1: FlexColumnWidth(2),
+              2: FlexColumnWidth(1),
+              3: FlexColumnWidth(1),
+              4: FlexColumnWidth(2),
+              5: FlexColumnWidth(2),
+            }),
+        SizedBox(height: 5.0),
         Container(
           alignment: Alignment.centerRight,
           child: Row(
@@ -191,25 +242,36 @@ class PdfInvoiceOldApi {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     buildText(
-                      title: 'Барлығы',
+                      title: 'Жиыны:',
                       titleStyle: TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
-                        font: nunitoBold
-                      ),
-                      value: "${invoice.total}тг",
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          font: nunitoBold),
+                      value: "${invoice.total}",
+                      unite: true,
+                    ),
+                    buildText(
+                      title: 'ҚҚ-сыз',
+                      titleStyle: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          font: nunitoBold),
+                      value: "-",
                       unite: true,
                     ),
                     SizedBox(height: 2 * PdfPageFormat.mm),
-                    Container(height: 1, color: PdfColors.grey400),
-                    SizedBox(height: 0.5 * PdfPageFormat.mm),
-                    Container(height: 1, color: PdfColors.grey400),
                   ],
                 ),
               ),
             ],
           ),
         ),
+        Text("Барлығы ${invoice.products.length} атау, ${invoice.total} KZT сомасында",
+        style: TextStyle(font: nunitoRegular)),
+        Text("Барлық төлем сомасы: ${invoice.total}",
+            style: TextStyle(font: nunitoBold)),
+        Divider(),
+        Text("Орындаушы _______________________/ ___________________ /", style: TextStyle(font: nunitoRegular)),
         SizedBox(height: 3 * PdfPageFormat.mm),
         Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -222,20 +284,20 @@ class PdfInvoiceOldApi {
                     "",
                     style: TextStyle(fontSize: 18),
                   ),
-                  Text(
-                    ""//invoice.paymentInstructions,
-                  ),
+                  Text("" //invoice.paymentInstructions,
+                      ),
                 ],
               ),
               Align(
-                alignment: Alignment.bottomRight,
-                child: Container()/*Image(
+                  alignment: Alignment.bottomRight,
+                  child:
+                      Container() /*Image(
                   MemoryImage(
                     invoice.signature.buffer.asUint8List(),
                   ),
                   height: 80,
                 ),*/
-              )
+                  )
             ]),
       ],
       footer: (context) => Column(
@@ -274,15 +336,15 @@ class PdfInvoiceOldApi {
     );
   }
 
-  static buildText({
-    required String title,
-    required String value,
-    double width = double.infinity,
-    TextStyle? titleStyle,
-    bool unite = false,
-    Font? font
-  }) {
-    final style = titleStyle ?? TextStyle(fontWeight: FontWeight.bold, font: font);
+  static buildText(
+      {required String title,
+      required String value,
+      double width = double.infinity,
+      TextStyle? titleStyle,
+      bool unite = false,
+      Font? font}) {
+    final style =
+        titleStyle ?? TextStyle(fontWeight: FontWeight.bold, font: font);
 
     return Container(
       width: width,
